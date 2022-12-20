ac54646758bc6f973374940115f7d0b5c5f89c98
ncarvalh <ncarvalh@student.42porto.com>
2022-12-20 18:20:00 +0000

    Script: added commands to fetch the number of physical and virtual processors and ramusage.

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,20 @@
+#! The system's own architecture
 arch=$(uname -a)
-phyproc=$(top -n1 | grep "%Cpu" | wc -l)
-virtproc=""
 
+#! The number of processor that actually exist physically
+phyproc=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+
+#! The number of total cores among the physical CPU's
+virtproc="$(grep -c processor /proc/cpuinfo)"
+
+#! Used RAM (in MB)
+ramusage=$(free -m | grep Mem | awk '{printf("%d/%d MB (%.2f%%)", $3, $2, $3/$2 * 100)}')
+
+#!
 
 echo "
 	#Architecture: $arch
 	#Physical CPUs: $phyproc
 	#Virtual CPUs: $virtproc
+	#RAM usage: $ramusage
 "
